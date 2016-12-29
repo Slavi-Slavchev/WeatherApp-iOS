f13c9556d996491ad22ed8678d58fd7009a200fc
Slavi Slavchev <slavi.lns@gmail.com>
2016-12-30 00:06:17 +0200

    add: base UI in Storyboard

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Slavi Slavchev on 12/29/16.
+//  Copyright © 2016 3Web Studio - Slavi Slavchev. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+
+    
+}
+
