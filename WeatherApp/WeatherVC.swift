5ba78467aed241f96d498b26ac84a9e35900634c
Slavi Slavchev <slavi.lns@gmail.com>
2016-12-30 00:53:09 +0200

    add: header IBOutlet

--- WeatherApp/WeatherVC.swift
+++ WeatherApp/WeatherVC.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class WeatherVC: UIViewController {
 
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLable: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
